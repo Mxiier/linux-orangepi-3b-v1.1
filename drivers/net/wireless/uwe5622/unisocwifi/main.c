3f6cfb6c20956f5223dc4e89807f04d42a451ee9
6x <lx0758@qq.com>
2025-11-09 16:55:37 +0800

    Enabled and Fix UWE5622 Driver

--- drivers/net/wireless/uwe5622/unisocwifi/main.c
+++ drivers/net/wireless/uwe5622/unisocwifi/main.c
@@ -945,11 +945,13 @@ static int sprdwl_set_mac(struct net_device *dev, void *addr)
 			vif->has_rand_mac = true;
 			memcpy(vif->random_mac, sa->sa_data, ETH_ALEN);
 			memcpy(dev->dev_addr, sa->sa_data, ETH_ALEN);
+			memcpy(dev->dev_addr_shadow, sa->sa_data, ETH_ALEN);
 		} else {
 			vif->has_rand_mac = false;
 			netdev_info(dev, "need clear random mac for sta/softap mode\n");
 			memset(vif->random_mac, 0, ETH_ALEN);
 			memcpy(dev->dev_addr, vif->mac, ETH_ALEN);
+			memcpy(dev->dev_addr_shadow, vif->mac, ETH_ALEN);
 		}
 	}
 	/*return success to pass vts test*/
